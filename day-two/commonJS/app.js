65880923804a4e495d958b23abc93d1a2048e9a4
Nishtha <n2036567@gmail.com>
2026-08-10 21:38:26 +0530

    day 3 http concept

--- day-two/commonJS/app.js
+++ day-two/commonJS/app.js
@@ -1,6 +1,20 @@
-const { name, age } = require('./data/myData');
-require('dotenv').config();
+// const { name, age } = require('./data/myData');
+// require('dotenv').config();
 
-console.log(`User Name: ${name}`);
-console.log(`User Age: ${age}`);
-console.log(`Server Port: ${process.env.PORT}`);
+// console.log(`User Name: ${name}`);
+// console.log(`User Age: ${age}`);
+// console.log(`Server Port: ${process.env.PORT}`);
+
+
+const http = require('http');
+const Port = process.env.PORT || 3000;
+
+const server = http.createServer((req,res)=>{
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<h1>Hello , World ! </h1>');
+    res.end();
+});
+
+server.listen(Port,()=>{
+    console.log(`Server is running on port ${Port}`);
+})
